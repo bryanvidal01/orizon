074abcca71cac2c840f3a6cac18dd69e19cc91c8
Bryan Vidal <bryan.vidal@hetic.net>
2017-06-01 00:09:18 +0200

    Intégration suite archive / single

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function(){
     jQuery('.push').click(function(){
         var el = jQuery(this);
-
         var timelineHome = new TimelineMax();
         // timelineHome.to(el, 1, {left:0, width: 100%, height: 100%});
         TweenLite.to(el, .5, {width:'100%', height: '100%', top: 0, left: 0, zIndex:2});
@@ -11,13 +10,26 @@ jQuery(document).ready(function(){
 
         setTimeout(function(){
             TweenLite.to(jQuery('.container-grid'), .2, {height: 0});
-            timelineHome.staggerFromTo(jQuery('.item-service'), .3,  {opacity:0, ease:Expo.easeIn},{opacity:1, ease:Cubic.easeIn}, .35);
         },1500);
     });
+
+    jQuery('.item-service').click(function(){
+        var el = jQuery(this);
+        el.addClass('open');
+
+        jQuery('.item-service').each(function(){
+            if(!jQuery(this).hasClass('open')){
+                var elItem = jQuery(this);
+                var timelineArchive = new TimelineMax();
+                timelineArchive.to(elItem, .5, {autoAlpha:0, ease:Cubic.easeOut});
+                timelineArchive.to(elItem, .5, {height:0, autoAlpha: 0, ease:Cubic.easeOut}, .4);
+            }
+        })
+
+    })
 });
 
 jQuery(window).on('load', function() {
-
     jQuery('.load').fadeOut(100);
     setTimeout(function(){
         animHome();
